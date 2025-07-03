19fb9f0280c8d4dc803b850ef089e86df444c5c3
João Marcelo <marcelojoao999543293@gmail.com>
2025-07-03 07:31:48 -0300

    Updat NovoArquivo2.cpp

--- NovoArquivo2.cpp
+++ NovoArquivo2.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#define PI 3.14159 //vari·vel constante
+#define PI 3.14159 //vari√°vel constante
 
 int main() {
-    float raio;//vari·vel real
+    float raio;
 
-    std::cout << "Digite o raio do cÌrculo: ";
+    std::cout << "Digite o raio do c√≠rculo: ";
     std::cin >> raio;
 
-    float area = PI * raio * raio; //vari·vel real com c·lculo
-    std::cout << "¡rea do cÌrculo: " << area << std::endl;
+    float area = PI * raio * raio; 
+    std::cout << "√Årea do c√≠rculo: " << area << std::endl;
 
     return 0;
 }
